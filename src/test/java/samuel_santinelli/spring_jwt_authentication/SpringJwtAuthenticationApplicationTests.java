76d9cc298a4f2104e553e43ea5612fbebc27b310
samuel-santinelli <samuelsantinelli@gmail.com>
2024-07-16 15:58:48 -0300

    feature: Generating the project with spring starter, and implementing the jwt

---
+++ src/test/java/samuel_santinelli/spring_jwt_authentication/SpringJwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package samuel_santinelli.spring_jwt_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
